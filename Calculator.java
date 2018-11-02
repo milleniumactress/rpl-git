bf701d84dd2ffe32e924f0ef721d9c91d280a007
MUHAMMAD RAVI <i16041@ftis>
2018-11-02 13:53:16 +0700

    udah push

---
+++ Calculator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author i16041
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public float plus(){
+        return 0;
+        
+    }
+    public float minus(){
+        return 0;
+        
+    }
+    
+    public float multiplication(){
+        return 0;
+        
+    }
+    
+    public float division(){
+        return 0;
+        
+    }
+    
+    
+}
