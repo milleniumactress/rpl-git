b37b2a63a9f7bc4ae1942b825e26ba31ac47ca38
KEVIN ARNOLD <i16038@ftis>
2018-11-09 10:28:32 +0700

    Mengganti kelas Calculator dengan Kalkulator

---
+++ Kalkulator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i16038
+ */
+public class Kalkulator {
+    public static void main(String[] args) {
+        
+    }
+    
+    public float plus(float a,float b){
+        return a+b;
+    }
+    
+    public float minus(float a,float b){
+        return a-b;
+    }
+    
+    //Firman yang edit
+    public float multiply(float a,float b){
+        return a*b;
+    }
+    
+    public float division(float a,float b){
+        
+        
+    }
+}
